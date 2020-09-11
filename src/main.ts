6de5b1a5cdbeb268aa48b418a982dc4c685f54aa
softprops <d.tangren@gmail.com>
2020-09-11 00:48:32 -0400

    refactor

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,49 @@
 import { setFailed, info } from "@actions/core";
 import AWS from "aws-sdk";
-import { AwsCredentials } from "./credentials";
-import { GitHubSecrets, encrypt } from "./secrets";
-import { input } from "./input";
+import { AwsCredentials, Credentials } from "./credentials";
+import { GitHubSecrets, Secrets, encrypt } from "./secrets";
+import { Input, input } from "./input";
 
-async function run() {
+async function rotate(
+  input: Input,
+  secrets: Secrets,
+  credentials: Credentials
+) {
+  const {
+    iamUserName,
+    githubAccessKeyIdName,
+    githubSecretAccessKeyName,
+  } = input;
+  const keys = await credentials.list();
+  if (keys.length == 2) {
+    setFailed(`AWS user ${iamUserName} already has 2 access keys`);
+    return;
+  }
+
+  info("Provisoning new access key");
+  const { AccessKeyId, SecretAccessKey } = await credentials.create();
+  console.log("Fetching repository public key");
+  const { key, key_id } = await secrets.publicKey();
+  info(`Upserting secret ${githubAccessKeyIdName}`);
+  await secrets.upsert(
+    githubAccessKeyIdName,
+    encrypt(AccessKeyId, key),
+    key_id
+  );
+  info(`Upserting secret ${githubSecretAccessKeyName}`);
+  await secrets.upsert(
+    githubSecretAccessKeyName,
+    encrypt(SecretAccessKey, key),
+    key_id
+  );
+  info("Deleting previous access key");
+  await credentials.delete(keys[0]);
+}
+
+async function main() {
   try {
-    const {
-      githubToken,
-      owner,
-      repo,
-      githubAccessKeyIdName,
-      githubSecretAccessKeyName,
-      iamUserName,
-    } = input(process.env);
-
+    const actionInput = input(process.env);
+    const { githubToken, owner, repo, iamUserName } = actionInput;
     const secrets = new GitHubSecrets(githubToken, owner, repo);
     const credentials = new AwsCredentials(
       new AWS.IAM(),
@@ -24,36 +53,12 @@ async function run() {
         )[1] ||
         ""
     );
-
-    const keys = await credentials.list();
-    if (keys.length == 2) {
-      setFailed(`AWS user ${iamUserName} already has 2 access keys`);
-      return;
-    }
-
-    info("Provisoning new access key");
-    const { AccessKeyId, SecretAccessKey } = await credentials.create();
-    console.log("Fetching repository public key");
-    const { key, key_id } = await secrets.publicKey();
-    info(`Upserting secret ${githubAccessKeyIdName}`);
-    await secrets.upsert(
-      githubAccessKeyIdName,
-      encrypt(AccessKeyId, key),
-      key_id
-    );
-    info(`Upserting secret ${githubSecretAccessKeyName}`);
-    await secrets.upsert(
-      githubSecretAccessKeyName,
-      encrypt(SecretAccessKey, key),
-      key_id
-    );
-    info("Deleting previous access key");
-    await credentials.delete(keys[0]);
+    await rotate(actionInput, secrets, credentials);
   } catch (error) {
     setFailed(error.message);
   }
 }
 
 if (require.main === module) {
-  run();
+  main();
 }
